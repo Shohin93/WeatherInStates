8d51feceacf9b9fb4902b84740242ca8f7e2bccd
Shohin Khamidjanov <skhamidjanov@verisign.com>
2017-12-24 20:36:01 -0500

    onformsubmit'

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -9,13 +9,16 @@ export default class SearchBar extends Component {
     }
 
     onInputChange(event) {
-        console.log(event.target.value);
         this.setState({term: event.target.value});
+    }
+
+    onFormSubmit(event) {
+        event.preventDefault();
     }
 
     render() {
         return (
-            <form className="input-group">
+            <form onSubmit={this.onFormSubmit} className="input-group">
                 <input 
                     placeholder="Get a five-day forecase in cities"
                     className="form-control"
